e36ce7514860974115690ddbb8daaa0fab3601bd
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-12-08 01:35:34 -0300

    node app

---
+++ node/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const config = {
+    host: 'database',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+try {
+  connection.query(`DROP TABLE people`)
+  connection.query(`CREATE TABLE people (name varchar(255))`)
+} catch(err) {
+  connection.query(`CREATE TABLE people (name varchar(255))`)
+}
+
+const sql = `INSERT INTO people(name) values ('Joao'), ('Pedro'), ('Maria')`;
+connection.query(sql);
+
+let data;
+
+connection.query("SELECT * FROM people", function (err, result, fields) {
+    data = result
+});
+
+connection.end();
+
+app.get('/', (req, res) => {
+ res.send(`<h1>Full Cycle Rocks!</h1> <h2>${data.map(item => ` ${item.name}`)}</h2>`);
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`))
